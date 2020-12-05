9849b5a69fbf732632cd5f5030e2df03c9a9447b
umut <ucok.umut@gmail.com>
2020-12-05 15:09:53 +0300

    Composite Design Pattern

---
+++ composite/Kumanya.java
@@ -0,0 +1,18 @@
+package structural.composite;
+
+import java.util.List;
+
+public class Kumanya implements Toplanabilir{
+    String adi;
+    List<Toplanabilir> itemList;
+
+    public Kumanya(String adi, List<Toplanabilir> itemList) {
+        this.adi = adi;
+        this.itemList = itemList;
+    }
+
+    @Override
+    public List<Toplanabilir> getToplamaList() {
+        return itemList;
+    }
+}
---
+++ composite/Paket.java
@@ -0,0 +1,18 @@
+package structural.composite;
+
+import java.util.List;
+
+public class Paket implements Toplanabilir{
+    List<Toplanabilir> urunList;
+    String paketAdi;
+
+    public Paket(List<Toplanabilir> urunList, String paketAdi) {
+        this.urunList = urunList;
+        this.paketAdi = paketAdi;
+    }
+
+    @Override
+    public List<Toplanabilir> getToplamaList() {
+        return urunList;
+    }
+}
